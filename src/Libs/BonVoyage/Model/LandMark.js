36bbcd5252362080a08759a3e77fc2c91efd4bce
UKayeF <nilseidmann@gmail.com>
2020-04-26 13:44:40 +0200

    add planet andromeda and beta centauri

--- src/Libs/BonVoyage/Model/LandMark.js
+++ src/Libs/BonVoyage/Model/LandMark.js
@@ -3,174 +3,199 @@ import GameState from './GameState';
 import Planet from './Planet';
 
 class LandMark {
-    static defaultList = [
-        {
-            distance: 100000,
-            visited: false,
-            action: function(store) {
-                if(store.playerFleet.shipsExpanded['208'].amount) {
-                    let data = {
-                        title: 'Met a Civil Fleet',
-                        type: 'custom',
-                        description: 'Hi. We would like to trade your very useful Colony Ship for our Esp. Probe, model SR-070. It survived more than 100 battles',
-                        actions: ['take', 'skip'],
-                        after: function (store, event, action) {
+  static defaultList = [
+    {
+      distance: 100000,
+      visited: false,
+      action: function (store) {
+        if (store.playerFleet.shipsExpanded['208'].amount) {
+          let data = {
+            title: 'Met a Civil Fleet',
+            type: 'custom',
+            description: 'Hi. We would like to trade your very useful Colony Ship for our Esp. Probe, model SR-070. It survived more than 100 battles',
+            actions: ['take', 'skip'],
+            after: function (store, event, action) {
 
-                            event.validActions.take = false;
-                            event.validActions.skip = false;
+              event.validActions.take = false;
+              event.validActions.skip = false;
 
-                            switch(action){
-                                case 'take':
-                                    let ships = {
-                                        '208' : store.playerFleet.shipsExpanded['208'].amount - 1,
-                                        '210' : store.playerFleet.shipsExpanded['210'].amount + 1
-                                    };
-                                    for(let idx in ships){
-                                        if(!ships.hasOwnProperty(idx)) continue;
+              switch (action) {
+                case 'take':
+                  let ships = {
+                    '208': store.playerFleet.shipsExpanded['208'].amount - 1,
+                    '210': store.playerFleet.shipsExpanded['210'].amount + 1
+                  };
+                  for (let idx in ships) {
+                    if (!ships.hasOwnProperty(idx)) continue;
 
-                                        store.playerFleet.updateShipAmountAndStats(
-                                            idx,
-                                            ships[idx],
-                                            window.bvConfig.shipData
-                                        );
-                                    }
-                                    store.playerFleet.techs['124']++;
-                                    event.description =
-                                        'We traded the ships. The probe came with a strange artifact that is being reviewed by our engineers';
-                                    store.pastEvents.push({time:store.playerFleet.timeUnit,
-                                        message:'Got a very special Esp. Probe','type':'info'});
+                    store.playerFleet.updateShipAmountAndStats(
+                      idx,
+                      ships[idx],
+                      window.bvConfig.shipData
+                    );
+                  }
+                  store.playerFleet.techs['124']++;
+                  event.description =
+                    'We traded the ships. The probe came with a strange artifact that is being reviewed by our engineers';
+                  store.pastEvents.push({
+                    time: store.playerFleet.timeUnit,
+                    message: 'Got a very special Esp. Probe', 'type': 'info'
+                  });
 
-                                    break;
-                                case 'skip':
-                                default:
-                                    store.pastEvents.push({time:store.playerFleet.timeUnit,
-                                        message:'Met a friendly caravan in the way','type':'info'});
-                                    event.description = 'We rejected the proposal due to a lack of Colony Ships in the area';
-                                    break;
-                            }
-                            setTimeout(function(){
-                                store.changeState(GameState.states.space);
-                            }, 5000);
-                        }
-                    };
-                    let state = store.eventManager.init('custom', data);
-                    return {state:state}
-                }
+                  break;
+                case 'skip':
+                default:
+                  store.pastEvents.push({
+                    time: store.playerFleet.timeUnit,
+                    message: 'Met a friendly caravan in the way', 'type': 'info'
+                  });
+                  event.description = 'We rejected the proposal due to a lack of Colony Ships in the area';
+                  break;
+              }
+              setTimeout(function () {
+                store.changeState(GameState.states.space);
+              }, 5000);
             }
-        },
-        {
-            distance: 76000,
-            visited: false,
-            action: function(store) {
-                let onlyProbes = true;
-                for(let i=0; i < Fleet.validShips.length; i++){
-                    let idx = Fleet.validShips[i];
-
-                    if( idx != 210 && store.playerFleet.shipsExpanded[idx].amount) {
-                        onlyProbes = false;
-                        break;
-                    }
-                }
-                if(onlyProbes){
-                    return {state: GameState.states.badEnding, data: GameState.endings.missingProbes};
-                }
-                return null;
-            }
-        },
-        {
-            distance: 74200,
-            visited: false,
-            mapData: {'id':'onigiri','symbol':'Ů','className':'text-info'},
-            action: function(store) {
-                return {state: GameState.states.planet, data: Planet.planets['onigiri']};
-            }
-        },
-        {
-            distance: 25800,
-            visited: false,
-            mapData: {'id':'tau-wg','symbol':'Φ','className':'text-info'},
-            action: function(store) {
-                return {state: GameState.states.planet, data: Planet.planets['tau-wg']};
-            }
-        },
-        {distance: 150000,
-        visited: false,
-        mapData: {'id': 'alpha-centauri', 'symbol': 'A', className: 'text-info'},
-        action: function (store) {
-            return {state: GameState.states.planet, data: Planet.planets['alpha-centauri']};
+          };
+          let state = store.eventManager.init('custom', data);
+          return {state: state}
         }
-        },
+      }
+    },
+    {
+      distance: 76000,
+      visited: false,
+      action: function (store) {
+        let onlyProbes = true;
+        for (let i = 0; i < Fleet.validShips.length; i++) {
+          let idx = Fleet.validShips[i];
 
-        {
-            distance: 5000,
-            visited: false,
-            action: function(store) {
-                if(store.playerFleet.techs['124'] > 5) {
-                    let data = {
-                        title: 'Darth Vader found our Fleet',
-                        description: 'Luke, please accept this Death Star, and together we will bring order to the galaxy',
-                        actions: ['take','flee'],
-                        after: function(store,event,action){
-                            event.validActions.take = false;
-                            event.validActions.flee = false;
-
-                            switch(action){
-                                case 'take':
-                                    //event.store.playerFleet.spaceCredits = 0;
-                                    store.playerFleet.updateShipAmountAndStats('214', 1, window.bvConfig.shipData);
-                                    event.description = 'Darth Vader\'s ship was a Hologram. But... is that a moon?';
-                                    store.pastEvents.push({time:store.playerFleet.timeUnit,
-                                        message:'Darth Vader gave us a gift','type':'success'});
-                                    break;
-                                case 'flee':
-                                default:
-                                    event.description = 'Darth Vader realizes Luke is not with us, and then we flee!';
-                                    store.pastEvents.push({time:store.playerFleet.timeUnit,
-                                        message:'We escaped from Darth Vader!','type':'info'});
-                                    break;
-                            }
-                            setTimeout(function(){
-                                store.changeState(GameState.states.space);
-                            }, 5000);
-
-                        }
-                    };
-                    let state = store.eventManager.init('custom', data);
-                    return {state:state}
-                }
-                return null;
-            }
-        },
-        {
-            distance: 3500,
-            visited: false,
-            action: function(store) {
-                let onlyProbes = true;
-                for(let i=0; i < Fleet.validShips.length; i++){
-                    let idx = Fleet.validShips[i];
-
-                    if( idx != 210 && store.playerFleet.shipsExpanded[idx].amount) {
-                        onlyProbes = false;
-                        break;
-                    }
-                }
-                if(onlyProbes){
-                    return {state: GameState.states.badEnding, data: GameState.endings.missingProbes};
-                }
-                return null;
-            }
-        },
-        {
-            distance: 0.000001,
-            visited: false,
-            mapData: {'id':'omega','symbol':'Ω','className':'text-success'},
-            action: function(store){
-                console.log('Ending!!');
-                return {state: GameState.states.goodEnding};
-            }
+          if (idx != 210 && store.playerFleet.shipsExpanded[idx].amount) {
+            onlyProbes = false;
+            break;
+          }
         }
-    ];
+        if (onlyProbes) {
+          return {state: GameState.states.badEnding, data: GameState.endings.missingProbes};
+        }
+        return null;
+      }
+    },
+    {
+      distance: 74200,
+      visited: false,
+      mapData: {'id': 'onigiri', 'symbol': 'Ů', 'className': 'text-info'},
+      action: function (store) {
+        return {state: GameState.states.planet, data: Planet.planets['onigiri']};
+      }
+    },
+    {
+      distance: 25800,
+      visited: false,
+      mapData: {'id': 'tau-wg', 'symbol': 'Φ', 'className': 'text-info'},
+      action: function (store) {
+        return {state: GameState.states.planet, data: Planet.planets['tau-wg']};
+      }
+    },
+    {
+      distance: 150000,
+      visited: false,
+      mapData: {'id': 'alpha-centauri', 'symbol': 'A', className: 'text-info'},
+      action: function (store) {
+        return {state: GameState.states.planet, data: Planet.planets['alpha-centauri']};
+      }
+    },
+    {
+      distance: 180000,
+      visited: false,
+      mapData: {'id': 'beta-centauri', 'symbol': 'B', className: 'text-info'},
+      action: function (store) {
+        return {state: GameState.states.planet, data: Planet.planets['beta-centauri']};
+      }
+    },
+    {
+      distance: 210000,
+      visited: false,
+      mapData: {'id': 'andromeda', 'symbol': 'A', className: 'text-info'},
+      action: function (store) {
+        return {state: GameState.states.planet, data: Planet.planets['andromeda']};
+      }
+    },
+    {
+      distance: 5000,
+      visited: false,
+      action: function (store) {
+        if (store.playerFleet.techs['124'] > 5) {
+          let data = {
+            title: 'Darth Vader found our Fleet',
+            description: 'Luke, please accept this Death Star, and together we will bring order to the galaxy',
+            actions: ['take', 'flee'],
+            after: function (store, event, action) {
+              event.validActions.take = false;
+              event.validActions.flee = false;
+
+              switch (action) {
+                case 'take':
+                  //event.store.playerFleet.spaceCredits = 0;
+                  store.playerFleet.updateShipAmountAndStats('214', 1, window.bvConfig.shipData);
+                  event.description = 'Darth Vader\'s ship was a Hologram. But... is that a moon?';
+                  store.pastEvents.push({
+                    time: store.playerFleet.timeUnit,
+                    message: 'Darth Vader gave us a gift', 'type': 'success'
+                  });
+                  break;
+                case 'flee':
+                default:
+                  event.description = 'Darth Vader realizes Luke is not with us, and then we flee!';
+                  store.pastEvents.push({
+                    time: store.playerFleet.timeUnit,
+                    message: 'We escaped from Darth Vader!', 'type': 'info'
+                  });
+                  break;
+              }
+              setTimeout(function () {
+                store.changeState(GameState.states.space);
+              }, 5000);
+
+            }
+          };
+          let state = store.eventManager.init('custom', data);
+          return {state: state}
+        }
+        return null;
+      }
+    },
+    {
+      distance: 3500,
+      visited: false,
+      action: function (store) {
+        let onlyProbes = true;
+        for (let i = 0; i < Fleet.validShips.length; i++) {
+          let idx = Fleet.validShips[i];
+
+          if (idx != 210 && store.playerFleet.shipsExpanded[idx].amount) {
+            onlyProbes = false;
+            break;
+          }
+        }
+        if (onlyProbes) {
+          return {state: GameState.states.badEnding, data: GameState.endings.missingProbes};
+        }
+        return null;
+      }
+    },
+    {
+      distance: 0.000001,
+      visited: false,
+      mapData: {'id': 'omega', 'symbol': 'Ω', 'className': 'text-success'},
+      action: function (store) {
+        console.log('Ending!!');
+        return {state: GameState.states.goodEnding};
+      }
+    }
+  ];
 }
+
 /* Screw calculus */
 
 export default LandMark;
--- src/Libs/BonVoyage/Model/Planet.js
+++ src/Libs/BonVoyage/Model/Planet.js
@@ -6,7 +6,9 @@ class Planet {
         "default": {name:"No Name",description:"Unknown Planet",actionStatus:0},
         "onigiri": {name:"Onigiri",description: "First Landmark of our Journey", actionStatus:0},
         "tau-wg": {name:"Tau-WG",description: "Main Confederate Planet", actionStatus:0},
-        "alpha-centauri": {name: "Alpha Centauri", description: "Closest Planet to Earth", actionStatus: 0}
+        "alpha-centauri": {name: "Alpha Centauri", description: "Closest Planet to Earth", actionStatus: 0},
+        "beta-centauri": {name: "Beta Centauri", description: "Second Closest Planet to Earth", actionStatus: 0},
+        "andromeda": {name: "Andromeda", description: "Here we are, Andromeda!", actionStatus: 0 },
     };
 
     @observable name = '';
