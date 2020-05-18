23e85d9228a450468850ec7215cd7c6c817d53a8
UKayeF <nilseidmann@gmail.com>
2020-05-18 18:53:46 +0200

    refmt

--- src/Components/Ships/SelectorComponent.jsx
+++ src/Components/Ships/SelectorComponent.jsx
@@ -5,27 +5,29 @@ import Fleet from '../../Libs/BonVoyage/Model/Fleet';
 
 class SelectorComponent extends Component {
 
-    validShipIds = Fleet.validConstructibleShips;
-    
-    render() {
+  validShipIds = Fleet.validConstructibleShips;
 
-        return (
-            <div>
-                <table className="tbl-ships">
-                    <tbody>
-                        <tr>
-                            {this.validShipIds.map((x, i) =>
-                                <SelectorItemComponent shipsExpanded={this.props.fleet.shipsExpanded}
-                                                       tryToAlterShipCount={this.props.tryToAlterShipCount}
-                                                       key={'shipInput-'+x}
-                                                       shipData={this.props.priceList[x]} shipId={x} />
-                            )}
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        )
-    }
+  render() {
+
+    return (
+      <div>
+        <table className="tbl-ships">
+          <tbody>
+          <tr>
+            {this.validShipIds.map((x, i) =>
+              <SelectorItemComponent
+                shipsExpanded={this.props.fleet.shipsExpanded}
+                tryToAlterShipCount={this.props.tryToAlterShipCount}
+                key={'shipInput-' + x}
+                shipData={this.props.priceList[x]} shipId={x}
+              />,
+            )}
+          </tr>
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
 
 export default SelectorComponent;
