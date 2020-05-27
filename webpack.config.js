ecf3a3b471095bffacb16dd91a67d82d84cdf1ec
UKayeF <nilseidmann@gmail.com>
2020-05-27 22:31:41 +0200

    optimize build process

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,53 @@
 var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   devtool: 'cheap-module-source-map',
   //devtool: 'eval',
   entry: [
-  //  'react-hot-loader/patch',
-  //  'webpack-dev-server/client?http://localhost:3000',
-  //  'webpack/hot/only-dev-server',
-    './src/index'
+    //  'react-hot-loader/patch',
+    //  'webpack-dev-server/client?http://localhost:3000',
+    //  'webpack/hot/only-dev-server',
+    './src/index',
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/dist/'
+    publicPath: '/dist/',
   },
   plugins: [
-  //  new webpack.HotModuleReplacementPlugin(),
+    //  new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
+        'NODE_ENV': JSON.stringify('production'),
+      },
     }),
     new HtmlWebpackPlugin({
       template: 'template.html',
     }),
+    new CopyWebpackPlugin(
+      {
+        patterns: [
+          {
+            from: 'styles',
+            to: 'styles',
+          },
+          {
+            from: 'sprites',
+            to: 'sprites',
+          },
+          {
+            from: 'scripts',
+            to: 'scripts',
+          },
+        ],
+      },
+    ),
   ],
   resolve: {
-    extensions: ['*', '.js', '.jsx']
+    extensions: ['*', '.js', '.jsx'],
   },
   module: {
     rules: [
