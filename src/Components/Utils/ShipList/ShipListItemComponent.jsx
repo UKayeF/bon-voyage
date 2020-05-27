66f8e64bcd3c51f1f6c3a9cd2f7e0199aaec1b9d
UKayeF <nilseidmann@gmail.com>
2020-05-27 20:43:20 +0200

    ditch table for divs and reformat documents

--- src/Components/Utils/ShipList/ShipListItemComponent.jsx
+++ src/Components/Utils/ShipList/ShipListItemComponent.jsx
@@ -4,33 +4,35 @@ import {getIconType, makeCSSName, shipsToCode} from '../../../utils/shipCodeMap'
 
 @observer
 class ShipListItemComponent extends Component {
-    render(){
-        let x = this.props.idx,
-            currentAmount = this.props.shipItem.amount,
-            changeAmount = this.props.shipItem.changes;
-        const shipName = makeCSSName(shipsToCode.get(parseInt(x)));
-        const iconType = getIconType(parseInt(x));
-        const className = `img small ${iconType} ${shipName}`
-        let changes = '';
-        if(changeAmount > 0){
-            changes = <span className="text-success">+{changeAmount}</span>
-        } else if(changeAmount < 0){
-            changes = <span className="text-error">{changeAmount}</span>
-        }
-
-        return(
-            <tr className={ (currentAmount || changeAmount) ? '' : 'hidden' }>
-                <td className="ship-desc half">
-                    { this.props.priceList[x].name }
-                </td><td className="one-fifth">
-                {/*<img src={ window.bvConfig.iconPath+x+'.gif' }*/}
-                {/*     height="20" width="20" />*/}
-                <div className={className}></div>
-            </td><td><span className="amount">{currentAmount}</span></td>
-                <td>{changes}</td>
-            </tr>
-        );
+  render(){
+    let x = this.props.idx,
+    currentAmount = this.props.shipItem.amount,
+    changeAmount = this.props.shipItem.changes;
+    const shipName = makeCSSName(shipsToCode.get(parseInt(x)));
+    const iconType = getIconType(parseInt(x));
+    const className = `img small ${iconType} ${shipName}`
+    let changes = '';
+    if(changeAmount > 0){
+      changes = <span className="text-success">+{changeAmount}</span>
+    } else if(changeAmount < 0){
+      changes = <span className="text-error">{changeAmount}</span>
     }
+
+    return (
+      <div className={ (currentAmount || changeAmount) ? 'ship-item' : 'hidden' }>
+        <div className="ship-desc half">
+          { this.props.priceList[x].name }
+        </div>
+        <div className="one-fifth">
+          <div className={className}></div>
+        </div>
+        <div>
+          <span className="amount">{currentAmount}</span>
+        </div>
+        <div>{changes}</div>
+      </div>
+    );
+  }
 }
 
 export default ShipListItemComponent;
--- src/Components/Utils/ShipListComponent.jsx
+++ src/Components/Utils/ShipListComponent.jsx
@@ -5,24 +5,22 @@ import ShipListItemComponent from './ShipList/ShipListItemComponent';
 
 class ShipListComponent extends Component {
 
-    validShips = Fleet.allBattleFleet; /* Try not to break the screen size */
-    
-    render() {
-        return (
-            <table className="tbl-active-ships">
-                <tbody>
-                    {this.validShips.map((x, i) =>
-                        <ShipListItemComponent
-                            key={"ship-list-"+this.props.module+'-'+x}
-                            idx={x}
-                            shipItem={this.props.fleet.shipsExpanded[x]}
-                            priceList={this.props.priceList}
-                        />
-                    )}
-                </tbody>
-            </table>
-        )
-    }
+  validShips = Fleet.allBattleFleet; /* Try not to break the screen size */
+
+  render() {
+    return (
+      <div className="tbl-active-ships ship-list">
+        {this.validShips.map((x, i) =>
+          <ShipListItemComponent
+            key={"ship-list-"+this.props.module+'-'+x}
+            idx={x}
+            shipItem={this.props.fleet.shipsExpanded[x]}
+            priceList={this.props.priceList}
+          />
+        )}
+      </div>
+    )
+  }
 }
 
 export default ShipListComponent;
