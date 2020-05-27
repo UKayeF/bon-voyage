c27a08cbc06a03ea1c4e0a0cb5af1278dc953120
UKayeF <nilseidmann@gmail.com>
2020-05-27 20:25:57 +0200

    reformat and replace table with divs

--- src/Components/Planet/ResearchLab/ResearchLabItemComponent.jsx
+++ src/Components/Planet/ResearchLab/ResearchLabItemComponent.jsx
@@ -7,49 +7,43 @@ import {getIconType, makeCSSName, shipsToCode} from '../../../utils/shipCodeMap'
 @observer
 class ResearchLabItemComponent extends Component {
 
-    @computed get currentPrice(){
-        const basePrice = ExchangeRate.resourcesToSpaceCredits(this.props.techData, ExchangeRate.NORMAL);
+  @computed get currentPrice(){
+    const basePrice = ExchangeRate.resourcesToSpaceCredits(this.props.techData, ExchangeRate.NORMAL);
 
-        return ResearchLabItemComponent.calcPrice(
-            basePrice,
-            this.props.techData.factor,
-            this.props.store.playerFleet.techs[this.props.techId]+1);
-    }
+    return ResearchLabItemComponent.calcPrice(
+      basePrice,
+      this.props.techData.factor,
+      this.props.store.playerFleet.techs[this.props.techId]+1);
+  }
 
-    render() {
-        const techId = this.props.techId;
-        // const imgUrl = window.bvConfig.iconPath+techId+'.gif';
-      const techName = makeCSSName(shipsToCode.get(parseInt(techId)));
-      const iconType = getIconType(parseInt(techId));
-      const className = `img ${iconType} ${techName}`
+  render() {
+    const techId = this.props.techId;
+    const techName = makeCSSName(shipsToCode.get(parseInt(techId)));
+    const iconType = getIconType(parseInt(techId));
+    const className = `img ${iconType} ${techName}`
 
 
-      return (
-            <td>
-                <span className="name">{this.props.techData.name}</span>
-              <div className={className}></div>
-                {/*<img src={imgUrl} height="48" width="48" />*/}
-                <span className="text-info">{this.props.store.playerFleet.techs[techId]}</span><br />
-                <span className="text-warning">§ {this.currentPrice}</span>
-                <table className="mini-buttons" cellPadding="0" cellSpacing="0">
-                    <tbody>
-                    <tr>
-                        <td><button className="text-success" onClick={this.addOne}>+1</button></td>
-                    </tr>
-                    </tbody>
-                </table>
-            </td>
-        )
-    }
-    
-    static calcPrice(basePrice, factor, desiredLevel){
+    return (
+      <div className='research-lab'>
+        <span className="name">{this.props.techData.name}</span>
+        <div className={className}></div>
+        <span className="text-info">{this.props.store.playerFleet.techs[techId]}</span><br />
+        <span className="text-warning">§ {this.currentPrice}</span>
+        <div className="mini-buttons" cellPadding="0" cellSpacing="0">
+          <button className="text-success" onClick={this.addOne}>+1</button>
+        </div>
+      </div>
+    )
+  }
+
+  static calcPrice(basePrice, factor, desiredLevel){
         //console.log("base price",(basePrice * Math.pow( factor, ( desiredLevel - 1 ) ) ) );
-        return (basePrice * Math.pow( factor, ( desiredLevel - 1 ) ) );
-    }
+    return (basePrice * Math.pow( factor, ( desiredLevel - 1 ) ) );
+  }
 
-    addOne = (event) => {
-        this.props.tryToPurchaseItem(this.props.techId);
-    }
+  addOne = (event) => {
+    this.props.tryToPurchaseItem(this.props.techId);
+  }
 }
 
 export default ResearchLabItemComponent;
