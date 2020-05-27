92302f08aa84113743b7f06e279a54cd503d2859
UKayeF <nilseidmann@gmail.com>
2020-05-27 20:17:21 +0200

    suffix numbers (like 1K, 4M etc.)

--- src/Components/ResourceTable.js
+++ src/Components/ResourceTable.js
@@ -1,19 +1,26 @@
 import React from 'react';
 
 const ResourceTable = ({metal = 0, crystal = 0, deuterium = 0}) => {
+
+  const suffixNumber = number => 
+    number > 1E9 ? `${Math.floor(number / 1E9)}B` :
+      number > 1E6 ? `${Math.floor(number / 1E6)}M` :
+        number > 1E3 ? `${Math.floor(number / 1E3)}K` :
+          number;
+
   return (
     <div className="tbl-resources fullwidth">
       <div>
         <div className="resource-img metal small" title='Metal'></div>
-        <div className='metal'>{metal}</div>
+        <div className='metal'>{suffixNumber(metal)}</div>
       </div>
       <div>
         <div className="resource-img crystal small" title='Crystal'></div>
-        <div className='crystal'>{crystal}</div>
+        <div className='crystal'>{suffixNumber(crystal)}</div>
       </div>
       <div>
         <div className='resource-img deuterium small' title='Deuterium'></div>
-        <div><span className="deuterium text-error">{deuterium}</span></div>
+        <div><span className="deuterium text-error">{suffixNumber(deuterium)}</span></div>
       </div>
     </div>
   );
