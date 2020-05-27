bf06bd09e7721ba996b83c860e53342255943373
UKayeF <nilseidmann@gmail.com>
2020-05-27 20:29:00 +0200

    ditch table and reformat file

--- src/Components/Planet/ResearchLabComponent.jsx
+++ src/Components/Planet/ResearchLabComponent.jsx
@@ -10,65 +10,65 @@ import Fleet from '../../Libs/BonVoyage/Model/Fleet';
 @observer
 class ResearchLabComponent extends Component {
 
-    @observable validating = false;
-    @observable success = false;
+  @observable validating = false;
+  @observable success = false;
 
-    validResearchLabTechs = Fleet.validResearchLabTechs;
-    
-    render() {
-        
-        return (
-            <div className={this.props.visibility?'':'hidden'}>
-                <h4>Research Lab</h4>
-                <p>You can borrow some of our technologies, for a price.</p>
-                <table className="tbl-ships tbl-ships-space">
-                    <tbody>
-                    <tr>
-                        {this.validResearchLabTechs.map((x, i) =>
-                            <ResearchLabItemComponent store={this.props.store} tryToPurchaseItem={this.tryToPurchaseItem}
-                                                   key={'researchLabInput-'+x}
-                                                   techData={this.props.priceList[x]} techId={x} />
-                        )}
-                    </tr>
-                    </tbody>
-                </table>
-                <div>
-                    <span className={this.success?'text-success':'hidden'}>Thanks for buying!</span>
-                    <span className={this.validating?'text-error':'hidden'}>Not enough Space Credits!</span>
-                </div>
-            </div>
-        );
+  validResearchLabTechs = Fleet.validResearchLabTechs;
+
+  render() {
+
+    return (
+      <div className={this.props.visibility?'':'hidden'}>
+        <h4>Research Lab</h4>
+        <p>You can borrow some of our technologies, for a price.</p>
+        <div className="tbl-ships tbl-ships-space">
+          <div>
+            {this.validResearchLabTechs.map((x, i) =>
+              <ResearchLabItemComponent store={this.props.store} tryToPurchaseItem={this.tryToPurchaseItem}
+                 key={'researchLabInput-'+x}
+                 techData={this.props.priceList[x]} techId={x}
+               />
+              )
+            }
+          </div>
+        </div>
+        <div>
+          <span className={this.success?'text-success':'hidden'}>Thanks for buying!</span>
+          <span className={this.validating?'text-error':'hidden'}>Not enough Space Credits!</span>
+        </div>
+      </div>
+    );
+  }
+
+  tryToPurchaseItem = (idx) => {
+
+    const basePrice = ExchangeRate.resourcesToSpaceCredits(this.props.priceList[idx], ExchangeRate.NORMAL);
+
+    this.validating = false;
+    this.success = false;
+
+    const price = ResearchLabItemComponent.calcPrice(
+      basePrice,
+      this.props.priceList[idx].factor,
+      this.props.store.playerFleet.techs[idx]+1);
+    if(price > this.props.store.playerFleet.spaceCredits){
+      this.validating = true;
+    } else {
+      this.props.store.playerFleet.techs[idx] += 1;
+      this.props.store.playerFleet.spaceCredits -= price;
+
+      this.success = true;
     }
 
-    tryToPurchaseItem = (idx) => {
+    setTimeout(() => {
+      this.validating = false;
+      this.success = false;
+    }, 3000);
 
-        const basePrice = ExchangeRate.resourcesToSpaceCredits(this.props.priceList[idx], ExchangeRate.NORMAL);
 
-        this.validating = false;
-        this.success = false;
-        
-        const price = ResearchLabItemComponent.calcPrice(
-            basePrice,
-            this.props.priceList[idx].factor,
-            this.props.store.playerFleet.techs[idx]+1);
-        if(price > this.props.store.playerFleet.spaceCredits){
-            this.validating = true;
-        } else {
-            this.props.store.playerFleet.techs[idx] += 1;
-            this.props.store.playerFleet.spaceCredits -= price;
+  };
 
-            this.success = true;
-        }
 
-        setTimeout(() => {
-            this.validating = false;
-            this.success = false;
-        }, 3000);
-  
-
-    };
-    
-    
 
 }
 
--- src/Components/Planet/Trader/TraderItemComponent.jsx
+++ src/Components/Planet/Trader/TraderItemComponent.jsx
@@ -7,54 +7,46 @@ import {getIconType, makeCSSName, shipsToCode} from '../../../utils/shipCodeMap'
 @observer
 class TraderItemComponent extends Component {
 
-    static getBaseTradePrice(idx, exchangeRate){
-        return Math.ceil(ExchangeRate.resourcesToSpaceCredits(window.bvConfig.shipData[idx], exchangeRate) * 0.3);
-    }
+  static getBaseTradePrice(idx, exchangeRate){
+    return Math.ceil(ExchangeRate.resourcesToSpaceCredits(window.bvConfig.shipData[idx], exchangeRate) * 0.3);
+  }
 
-    getTradePrice(){
-        return TraderItemComponent.getBaseTradePrice(this.props.shipId, ExchangeRate.NORMAL);
-    }
+  getTradePrice(){
+    return TraderItemComponent.getBaseTradePrice(this.props.shipId, ExchangeRate.NORMAL);
+  }
 
-    render() {
+  render() {
 
-        const shipId = this.props.shipId;
-        // const imgUrl = window.bvConfig.iconPath+shipId+'.gif';
-        const shipName = makeCSSName(shipsToCode.get(parseInt(shipId)));
-        const iconType = getIconType(parseInt(shipId));
-        const className = `img ${iconType} ${shipName}`
+    const shipId = this.props.shipId;
+    const shipName = makeCSSName(shipsToCode.get(parseInt(shipId)));
+    const iconType = getIconType(parseInt(shipId));
+    const className = `img ${iconType} ${shipName}`
 
-        return (
-            <td>
-                <span className="name">{this.props.shipData.name}</span>
-                <div className={className}></div>
-                {/*<img src={imgUrl} height="48" width="48" />*/}
-                <span className="text-info">{this.props.store.playerFleet.shipsExpanded[shipId].amount}</span><br />
-                <span className="text-error">§ {this.getTradePrice()}</span>
-                <table className="mini-buttons" cellPadding="0" cellSpacing="0">
-                    <tbody>
-                    <tr>
-                        <td><button className="text-error" onClick={this.sellOne}>-1</button></td>
-                    </tr>
-                    <tr>
-                        <td><button className="text-error" onClick={this.sellTen}>-10</button></td>
-                    </tr>
-                    </tbody>
-                </table>
-            </td>
-        )
-    }
+    return (
+      <div>
+        <span className="name">{this.props.shipData.name}</span>
+        <div className={className}></div>
+        <span className="text-info">{this.props.store.playerFleet.shipsExpanded[shipId].amount}</span><br />
+        <span className="text-error">§ {this.getTradePrice()}</span>
+        <div className="mini-buttons" cellPadding="0" cellSpacing="0">
+          <button className="text-error" onClick={this.sellOne}>-1</button>
+          <button className="text-error" onClick={this.sellTen}>-10</button>
+        </div>
+      </div>
+    )
+  }
 
-    sellOne = (event) => {
-        this.sellAmount(1);
-    };
+  sellOne = (event) => {
+    this.sellAmount(1);
+  };
 
-    sellTen = (event) => {
-        this.sellAmount(10);
-    };
+  sellTen = (event) => {
+    this.sellAmount(10);
+  };
 
-    sellAmount(amount){
-        this.props.tryToSellShip(this.props.shipId, amount);
-    }
+  sellAmount(amount){
+    this.props.tryToSellShip(this.props.shipId, amount);
+  }
 }
 
 export default TraderItemComponent;
