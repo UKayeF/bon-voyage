95874e384fbbaf6581b036bd5509d814676417dd
UKayeF <nilseidmann@gmail.com>
2020-05-28 14:35:06 +0200

    >= instead of >

--- src/Components/ResourceTable.js
+++ src/Components/ResourceTable.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const ResourceTable = ({metal = 0, crystal = 0, deuterium = 0}) => {
 
-  const suffixNumber = number => 
-    number > 1E9 ? `${Math.floor(number / 1E9)}B` :
-      number > 1E6 ? `${Math.floor(number / 1E6)}M` :
-        number > 1E3 ? `${Math.floor(number / 1E3)}K` :
+  const suffixNumber = number =>
+    number >= 1E9 ? `${Math.floor(number / 1E9)}B` :
+      number >= 1E6 ? `${Math.floor(number / 1E6)}M` :
+        number >= 1E3 ? `${Math.floor(number / 1E3)}K` :
           number;
 
   return (
