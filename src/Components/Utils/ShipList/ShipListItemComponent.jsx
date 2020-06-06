42bbc27a72a102565392944f62fec8b8f7b5a07a
UKayeF <nilseidmann@gmail.com>
2020-06-07 00:35:01 +0200

    change ship list layout in space view

--- src/Components/Utils/ShipList/ShipListItemComponent.jsx
+++ src/Components/Utils/ShipList/ShipListItemComponent.jsx
@@ -1,31 +1,34 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
-import {getIconType, makeCSSName, shipsToCode} from '../../../utils/shipCodeMap';
+import { getIconType, makeCSSName, shipsToCode } from '../../../utils/shipCodeMap';
 
 @observer
 class ShipListItemComponent extends Component {
-  render(){
+  render() {
     let x = this.props.idx,
-    currentAmount = this.props.shipItem.amount,
-    changeAmount = this.props.shipItem.changes;
+      currentAmount = this.props.shipItem.amount,
+      changeAmount = this.props.shipItem.changes;
     const shipName = makeCSSName(shipsToCode.get(parseInt(x)));
     const iconType = getIconType(parseInt(x));
     const className = `img small ${iconType} ${shipName}`
     let changes = '';
-    if(changeAmount > 0){
+    if (changeAmount > 0) {
       changes = <span className="text-success">+{changeAmount}</span>
-    } else if(changeAmount < 0){
+    }
+    else if (changeAmount < 0) {
       changes = <span className="text-error">{changeAmount}</span>
     }
 
     return (
-      <div className={ (currentAmount || changeAmount) ? 'ship-item' : 'hidden' }>
+      <div className={(currentAmount || changeAmount) ? 'ship-item' : 'hidden'}>
+        <div className='ship-name-amount'>
         <span className='ship-desc'>
-          { this.props.priceList[x].name }
+          {this.props.priceList[x].name}
         </span>
-        <span className='amount'>{currentAmount}</span>
-        <div>{changes}</div>
-        <div className=''>
+          <span className='amount'>{currentAmount}</span>
+          <div>{changes}</div>
+        </div>
+        <div className='img-container'>
           <div className={className}></div>
         </div>
       </div>
